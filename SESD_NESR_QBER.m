09bf3a407d485e70a28a62246a85e2cd2d03ad5e
HBS <137296863+Hao-B-Shu@users.noreply.github.com>
2025-10-17 13:36:27 +0800

    Add files via upload

--- SESD_NESR_QBER.m
+++ SESD_NESR_QBER.m
@@ -3,24 +3,24 @@ function [SESD,NESR,QBER,Pkn,Qkn]=SESD_NESR_QBER(varargin)%eta,d,ep,ec,u,n,k,P,t
 Para=inputParser;
 addOptional(Para,'eta',0.78);
 addOptional(Para,'d',10^(-7));
-addOptional(Para,'ep',0.002);
+addOptional(Para,'Q',0.002);
 addOptional(Para,'ec',0);
-addOptional(Para,'y',1-exp(-5));
 addOptional(Para,'n',3);
 addOptional(Para,'k',1);
 addOptional(Para,'P',0.99);
+addOptional(Para,'p1',0.99);
 addOptional(Para,'t',10^(-5));
 addOptional(Para,'dig',32);
 parse(Para,varargin{:});
 
 eta=Para.Results.eta;
 d=Para.Results.d;
-ep=Para.Results.ep;
+Q=Para.Results.Q;
 ec=Para.Results.ec;
-y=Para.Results.y;
+P=Para.Results.P;
+p1=Para.Results.p1;
 n=Para.Results.n;
 k=Para.Results.k;
-P=Para.Results.P;
 t=Para.Results.t;
 dig=Para.Results.dig;
 
@@ -28,37 +28,59 @@ digits(dig);
 t=vpa(t);
 d=vpa(d);
 P=vpa(P);
-ep=vpa(ep);
+p1=vpa(p1);
+Q=vpa(Q);
 ec=vpa(ec);
 eta=vpa(eta);
 n=vpa(n);
 k=vpa(k);
-y=vpa(y);
 
-Ps=(1-y)*(P*(eta+(1-eta)*d)+(1-P)*d)+y*d;
-Qs=(1-y)*(ep*(eta+(1-eta)*d)+(1-ep)*d)+y*d;
+Ps=P*(eta+(1-eta)*d)+(1-P)*d;
+Qs=Q*(eta+(1-eta)*d)+(1-Q)*d;
 
-Pkn = vpa(0);
-for m = k:n
-    Pkn = Pkn + nchoosek(n,m)*(Ps^m)*(1-Ps)^(n-m);
+if n==0
+    Pknmain=1;
+else
+    Pknmain = vpa(0);
+    for m = k:n
+        Pknmain = Pknmain + nchoosek(n,m)*(Ps^m)*(1-Ps)^(n-m);
+    end
 end
-%Pkn = 1 - binocdf(k-1, n, Ps);
 
-Qkn = vpa(0);
-for m = k:n
-    Qkn = Qkn + nchoosek(n,m)*(Qs^m)*(1-Qs)^(n-m);
+if n==0
+    Qkn=1;
+else
+    Qkn = vpa(0);
+    for m = k:n
+        Qkn = Qkn + nchoosek(n,m)*(Qs^m)*(1-Qs)^(n-m);
+    end
 end
-%Qkn = 1 - binocdf(k-1, n, Qs);
 
+if n==0
+    Pkn=1;
+else
+    Pkn = p1^(n-1)*Pknmain;
+    for i = 1:(n-1)
+            inner_sum = 0;
+            for j1 = 0:i
+                for j2 = 0:(n-i)
+                    if (j1 + j2) >= k
+                        term = nchoosek(i, j1)*Ps^j1*(1-Ps)^(i-j1)*nchoosek(n-i, j2)*Qs^j2*(1-Qs)^(n-i-j2);
+                        inner_sum = inner_sum + term;
+                    end
+                end
+            end
+            Pkn = Pkn + p1^(i-1)*(1-p1)*inner_sum;
+    end
+end
 
-NESR=(t*Pkn)./(t*Pkn+(1-t)*Qkn);
+SESD=t*Pkn+(1-t)*Qkn;
+NESR=(t*p1^(n)*Pknmain)./SESD;
 
 ct=(1-ec)*(eta+(1-eta)*d)*(1-d)+ec*(1-eta)*d*(1-d);
 et=ec*(eta+(1-eta)*d)*(1-d)+(1-ec)*(1-eta)*d*(1-d);
 cl=d*(1-d);
 el=d*(1-d);
 QBER=(NESR*et+(1-NESR)*el)./(NESR*(et+ct)+(1-NESR)*(el+cl));
-
-SESD=t*Pkn+(1-t)*Qkn;
 
 end
